a1e5100b8732b8095532f76559aec44db3e0a63d
AhnJoonSung <tjdtna01@naver.com>
2023-03-16 06:49:38 +0900

    refactoring : hashMap 대신 26칸 짜리 배열을 두어 두 문자열의 문자수를 각각 카운트하고 이를 비교하는 식으로 변경, 바로 System.out.println하지 않고 StringBuilder를 사용하여 하나의 문자열로 만들어 한번의 print로 출력하는 식으로 변경, IOException 이외의 예외도 처리하기 위해 Exception handling으로 변경

--- BOJ/src/array/b2_11328_strfry/Main.java
+++ BOJ/src/array/b2_11328_strfry/Main.java
@@ -2,46 +2,43 @@ package array.b2_11328_strfry;
 
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.util.HashMap;
+import java.util.Arrays;
 
 public class Main {
-    static String POSSIBLE = "Possible";
-    static String IMPOSSIBLE = "Impossible";
+    private static final String POSSIBLE = "Possible";
+    private static final String IMPOSSIBLE = "Impossible";
 
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         try {
             int n = Integer.parseInt(br.readLine());
-            String s1, s2;
-            HashMap<Character, Integer> hashMap = new HashMap<>();
+            StringBuilder sb = new StringBuilder();
             for (int i = 0; i < n; i++) {
                 String[] input = br.readLine().split(" ");
-                s1 = input[0];
-                s2 = input[1];
-                setMap(s1, hashMap, 1);
-                setMap(s2, hashMap, -1);
-                if (hashMap.isEmpty())
-                    System.out.println(POSSIBLE);
-                else
-                    System.out.println(IMPOSSIBLE);
-                hashMap.clear();
-            }
-            br.close();
+                String str1 = input[0];
+                String str2 = input[1];
 
-        } catch (IOException e) {
+                if (isPossible(str1, str2))
+                    sb.append(POSSIBLE + "\n");
+                else
+                    sb.append(IMPOSSIBLE + "\n");
+            }
+            System.out.print(sb);
+            br.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static void setMap(String s, HashMap<Character, Integer> hashMap, int cal) {
-        for (int i = 0; i < s.length(); i++) {
-            Character c = s.charAt(i);
-            int newValue = hashMap.getOrDefault(c, 0) + cal;
-            if (newValue == 0)
-                hashMap.remove(c);
-            else
-                hashMap.put(c, newValue);
+    private static boolean isPossible(String str1, String str2) {
+        return (Arrays.equals(getCnt(str1), getCnt(str2)));
+    }
+
+    private static int[] getCnt(String str) {
+        int[] cnt = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            cnt[str.charAt(i) - 'a']++;
         }
+        return (cnt);
     }
 }
