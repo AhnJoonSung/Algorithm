fee99ec928e034ea065029ab960d4787a58fb106
AhnJoonSung <tjdtna01@naver.com>
2023-03-15 23:45:00 +0900

    refactoring : change name : set -> cnt

--- BOJ/src/array/s5_1475_방번호/Main.java
+++ BOJ/src/array/s5_1475_방번호/Main.java
@@ -6,19 +6,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String n = sc.nextLine();
-        int[] set = new int[10];
+        int[] cnt = new int[10];
 
         for (int i = 0; i < n.length(); i++) {
             int digit = n.charAt(i) - '0';
             if (digit == 6)
-                set[9]++;
+                cnt[9]++;
             else
-                set[digit]++;
+                cnt[digit]++;
         }
-        int max = (set[9] + 1) / 2;
-        for (int i = 0; i < set.length - 1; i++) {
-            if (set[i] > max)
-                max = set[i];
+        int max = (cnt[9] + 1) / 2;
+        for (int i = 0; i < cnt.length - 1; i++) {
+            if (cnt[i] > max)
+                max = cnt[i];
         }
         System.out.println(max);
     }
