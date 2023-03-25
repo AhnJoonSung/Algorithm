139b5b7fc7b76cf894bcfa1d9daea712b9d5f333
AhnJoonSung <tjdtna01@naver.com>
2023-03-25 23:18:05 +0900

    임시 전송용

---
+++ BOJ/src/stack/g4_17298_오큰수/Main.java
@@ -0,0 +1,12 @@
+package stack.g4_17298_오큰수;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+    }    
+}
--- BOJ/src/stack/g5_2493_탑/Main.java
+++ BOJ/src/stack/g5_2493_탑/Main.java
@@ -3,11 +3,39 @@ package stack.g5_2493_탑;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.Stack;
+import java.util.LinkedList;
 
 public class Main {
+    static final int MAX = 100000000;
     public static void main(String[] args) throws IOException{
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int n = Integer.parseInt(br.readLine());
+            String[] inputs = br.readLine().split(" ");
+            LinkedList<String> result = new LinkedList<>();
 
+            Stack<Building> buildings = new Stack<>();
+            buildings.push(new Building(0, MAX + 1));
+            for (int i = 1; i <= n; i++) {
+                int height = Integer.parseInt(inputs[i - 1]);
+                
+                while (buildings.peek().height < height) {
+                    buildings.pop();
+                }
+                result.add(Integer.toString(buildings.peek().index));
+                buildings.push(new Building(i, height));
+            }
+            System.out.println(String.join(" ", result));
         }
+    }
+}
+
+class Building {
+    int index;
+    int height;
+
+    public Building(int index, int height) {
+        this.index = index;
+        this.height = height;
     }
 }
