a7d9199bd4ff1a573514633a9fe27481c4116ceb
AhnJoonSung <tjdtna01@naver.com>
2023-04-02 21:44:09 +0900

    리팩토링 : 처음코드가 시간초과가 나서 큐를 써서 토마토만 순회하도록 변경, 함수 인자로 넘기니 스코프 밖에선 변경이 적용이 안돼 전역변수로 변경

--- BOJ/src/bfs/g5_7569_토마토/Main.java
+++ BOJ/src/bfs/g5_7569_토마토/Main.java
@@ -1,5 +1,7 @@
 package bfs.g5_7569_토마토;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
@@ -8,6 +10,8 @@ public class Main {
     static final int[] dk = {0, 0, -1, 1, 0, 0};
 
     static int m, n, h;
+    static int[][][] box;
+    static Queue<Tomato> q;
 
     public static void main(String[] args) {
         // 6개 방향
@@ -17,68 +21,49 @@ public class Main {
         n = sc.nextInt();
         h = sc.nextInt();
 
-        int[][][]box = new int[h][n][m];
-        int need_change = 0;
+        box = new int[h][n][m];
+        q = new LinkedList<>();
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < n; j++) {
                 for (int k = 0; k < m; k++) {
                     box[i][j][k] = sc.nextInt();
-                    if (box[i][j][k] == 0)
-                        need_change++;
+                    if (box[i][j][k] == 1)
+                        q.add(new Tomato(i,j,k,0));
                 }
             }
         }
-
-        if (need_change == 0) {
-            System.out.println(0);
-            return ;
-        }
-        int day = 1;
-        int change_cnt = 0;
-        while (true) {
-            int temp = bfs(box);
-            change_cnt += temp;
-            if (change_cnt == need_change) {
-                System.out.println(day);
-                return ;
-            }
-            if (temp == 0) {
-                System.out.println(-1);
-                return ;
-            }
-            day++;
-        }
-
-    }
-
-    public static int bfs(int[][][] box) {
-        int change_cnt = 0;
-        boolean[][][] visit = new boolean[h][n][m];
-
+        int day = bfs();
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < n; j++) {
                 for (int k = 0; k < m; k++) {
-                    if (!isValid(i, j, k)) continue;
-                    if (box[i][j][k] != 1) continue;
-                    if (visit[i][j][k]) continue;
-
-                    visit[i][j][k] = true;
-                    Position cur = new Position(i, j, k);
-                    for (int dir = 0; dir < 6; dir++) {
-                        int ni = cur.i + di[dir];
-                        int nj = cur.j + dj[dir];
-                        int nk = cur.k + dk[dir];
-
-                        if (!isValid(ni, nj, nk)) continue;
-                        if (box[ni][nj][nk] != 0) continue;
-                        box[ni][nj][nk] = 1;
-                        visit[ni][nj][nk] = true;
-                        change_cnt++;
+                    if (box[i][j][k] == 0) {
+                        System.out.println(-1);
+                        return ;
                     }
                 }
             }
         }
-        return (change_cnt);
+        System.out.println(day);
+    }
+
+    public static int bfs() {
+        int max_day = 0;
+        while (!q.isEmpty()) {
+            Tomato cur = q.poll();
+            if (cur.day > max_day)
+                max_day = cur.day;
+            for (int dir = 0; dir < 6; dir++) {
+                int i = cur.i + di[dir];
+                int j = cur.j + dj[dir];
+                int k = cur.k + dk[dir];
+
+                if (!isValid(i, j, k)) continue;
+                if (box[i][j][k] != 0) continue;
+                box[i][j][k] = 1;
+                q.add(new Tomato(i, j, k, cur.day + 1));
+            }
+        }
+        return (max_day);
     }
 
     public static boolean isValid(int i, int j, int k) {
@@ -92,12 +77,14 @@ public class Main {
     }
 }
 
-class Position {
+class Tomato {
     int i, j, k;
+    int day;
 
-    public Position(int i, int j, int k) {
+    public Tomato(int i, int j, int k, int day) {
         this.i = i;
         this.j = j;
         this.k = k;
+        this.day = day;
     }
 }
