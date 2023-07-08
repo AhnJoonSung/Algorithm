c38a46815151b61d5a96fbbf1c5e939c6c51fa4f
AhnJoonSung <tjdtna01@naver.com>
2023-07-08 15:16:10 +0900

    test

--- BOJ/src/silver/s2_15663_N과M9/Main.java
+++ BOJ/src/silver/s2_15663_N과M9/Main.java
@@ -3,17 +3,27 @@ package silver.s2_15663_N과M9;
 import java.util.Scanner;
 
 public class Main {
+    static int n, m;
     static int[] num;
+    static boolean[] visited;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        n = sc.nextInt();
+        m = sc.nextInt();
 
         num = new int[n];
+        visited = new boolean[n];
         for (int i = 0; i < n; i++)
             num[i] = sc.nextInt();
-        test
+        Arrays.sort(nums);
+
+    }
+
+    public static void comb(int start, int depth) {
+        if (depth == m) {
+
+        }
     }
 }
