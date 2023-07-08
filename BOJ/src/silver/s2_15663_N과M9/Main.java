cf2c0081cfac8e1df0a570dad9448e32d1fc6501
AhnJoonSung <tjdtna01@naver.com>
2023-07-08 16:02:29 +0900

    first code

--- BOJ/src/silver/s2_15663_N과M9/Main.java
+++ BOJ/src/silver/s2_15663_N과M9/Main.java
@@ -1,11 +1,14 @@
 package silver.s2_15663_N과M9;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     static int n, m;
-    static int[] num;
+    static int[] nums;
+    static int[] ans;
     static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -13,17 +16,33 @@ public class Main {
         n = sc.nextInt();
         m = sc.nextInt();
 
-        num = new int[n];
+        nums = new int[n];
+        ans = new int[m];
         visited = new boolean[n];
         for (int i = 0; i < n; i++)
-            num[i] = sc.nextInt();
+            nums[i] = sc.nextInt();
         Arrays.sort(nums);
-
+        comb(0);
+        System.out.print(sb);
     }
 
-    public static void comb(int start, int depth) {
+    public static void comb(int depth) {
         if (depth == m) {
+            for (int v : ans)
+                sb.append(v).append(" ");
+            sb.append("\n");
+            return;
+        }
 
+        int lastNum = -1;
+        for (int i = 0; i < n; i++) {
+            if (visited[i] || lastNum == nums[i])
+                continue;
+            lastNum = nums[i];
+            visited[i] = true;
+            ans[depth] = nums[i];
+            comb(depth + 1);
+            visited[i] = false;
         }
     }
 }
