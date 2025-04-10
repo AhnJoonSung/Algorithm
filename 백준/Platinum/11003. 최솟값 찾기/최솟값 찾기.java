04f5e3a02e92d5a02938c6419fb1a44eb08182c9
Ahn Joon Sung <tjdtna01@naver.com>
2025-04-10 11:04:37 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2208 ms, Memory: 728084 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -12,73 +12,31 @@ public class Main {
         int L = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        int[] nums = new int[N + 1];
-        for (int i = 1; i <= N; i++)
-            nums[i] = Integer.parseInt(st.nextToken());
-
-        SegmentTree tree = new SegmentTree(nums);
-
+        Deque<Node> deq = new LinkedList<>();
         StringBuilder sb = new StringBuilder();
-        Queue<Integer> q = new LinkedList<>();
-        int min = Integer.MAX_VALUE;
         for (int i = 1; i <= N; i++) {
-            int start = Math.max((i - L + 1), 1);
-            if (L == 1)
-                min = nums[i];
-            else {
-                q.add(nums[i]);
-                if ((start > 1) && (min == q.poll()))  
-                    min = tree.queryMin(start, i);
-                else
-                    min = Math.min(min, nums[i]);
-            }
-            sb.append(min).append(" ");
+            int current = Integer.parseInt(st.nextToken());
+
+            while (!deq.isEmpty() && deq.peekLast().value > current)
+                deq.pollLast();
+
+            deq.add(new Node(i, current));
+            
+            if (deq.peekFirst().idx < (i - L + 1))
+                deq.pollFirst();
+
+            sb.append(deq.peekFirst().value).append(" ");
         }
 
         System.out.println(sb);
     }
 }
 
-class SegmentTree {
-    int[] minTree;
-    int[] arr;
-    int n;
+class Node {
+    int idx, value;
 
-    public SegmentTree(int[] arr) {
-        this.arr = arr;
-        n = arr.length - 1;
-        minTree = new int[4 * n];
-        initMin(1, 1, n);
-    }
-
-    private int initMin(int node, int start, int end) {
-        if (start == end) {
-            return minTree[node] = arr[start];
-        }
-
-        int mid = (start + end) / 2;
-        
-        return minTree[node] = Math.min(
-            initMin(node * 2, start, mid),
-            initMin(node * 2 + 1, mid + 1, end)
-        );
-    }
-
-    public int queryMin(int left, int right) {
-        return recursiveQueryMin(1, 1, n, left, right);
-    }
-
-    private int recursiveQueryMin(int node, int start, int end, int left, int right) {
-        if (right < start || end < left)
-            return Integer.MAX_VALUE;
-        if (left <= start && end <= right)
-            return minTree[node];
-
-        int mid = (start + end) / 2;
-
-        return Math.min(
-            recursiveQueryMin(node * 2, start, mid, left, right),
-            recursiveQueryMin(node * 2 + 1, mid + 1, end, left, right)
-        );
+    public Node(int idx, int value) {
+        this.idx = idx;
+        this.value = value;
     }
 }
