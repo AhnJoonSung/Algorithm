eac174577923506b042b76b26351d2b020e55fa1
Ahn Joon Sung <tjdtna01@naver.com>
2025-04-17 09:39:38 +0900

    [Gold I] Title: 낚시왕, Time: 848 ms, Memory: 75660 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.java
+++ 백준/Gold/17143. 낚시왕/낚시왕.java
@@ -7,80 +7,48 @@ public class Main {
         int r = sc.nextInt();
         int c = sc.nextInt();
 
-        List<Shark>[][] map = new List[r+1][c+1];
-        for (int i = 1; i <= r; i++) {
-            for (int j = 1; j <= c; j++) {
-                map[i][j] = new ArrayList<Shark>();
-            }
-        }
+        Shark [][] map = new Shark[r+1][c+1];
 
         int m = sc.nextInt();
 
-        List<Shark> sharks = new ArrayList<>();
         while (m-- > 0) {
             int i = sc.nextInt();
             int j = sc.nextInt();
-            Shark shark = new Shark(i, j, sc.nextInt(), sc.nextInt(), sc.nextInt(), map);
-            sharks.add(shark);
-            map[i][j].add(shark);
+            Shark shark = new Shark(i, j, sc.nextInt(), sc.nextInt(), sc.nextInt());
+            map[i][j] = shark;
         }
 
-        King king = new King(map, sharks);
-        while (king.move()) {
-            king.fishing();
+        King king = new King();
+        while (king.move(map)) {
+            king.fishing(map);
 
-            for (Shark shark: sharks)
-                shark.move();
+            Shark[][] newMap = new Shark[r+1][c+1];
 
-            eatOther(map, sharks);
+            for (int i = 1; i <= r; i++) {
+                for (int j = 1; j <= c; j++) {
+                    if (map[i][j] != null) {
+                        map[i][j].move(newMap);
+                    }
+                }
+            }
+
+            map = newMap;
         }
 
         System.out.println(king.score);
-    }
-
-    static void eatOther(List<Shark>[][] map, List<Shark> sharks) {
-        int r = map.length - 1;
-        int c = map[0].length - 1;
-
-        for (int i = 1; i <= r; i++) {
-            for (int j = 1; j <= c; j++) {
-                if (map[i][j].size() >= 2)
-                    eat(map[i][j], sharks);
-            }
-        }
-    }
-
-    static void eat(List<Shark> cell, List<Shark> sharks) {
-        Shark biggest = cell.get(0);
-        for (int i = 1; i < cell.size(); i++) {
-            Shark shark = cell.get(i);
-            if (shark.size < biggest.size) {
-                sharks.remove(shark);
-            } else {
-                sharks.remove(biggest);
-                biggest = shark;
-            }
-        }
-
-        cell.clear();
-        cell.add(biggest);
     }
 }
 
 class King {
     int j;
     int score;
-    List<Shark>[][] map;
-    List<Shark> sharks;
 
-    public King(List<Shark>[][] map, List<Shark> sharks) {
+    public King() {
         this.j = 0;
         this.score = 0;
-        this.map = map;
-        this.sharks = sharks;
     }
 
-    public boolean move() {
+    public boolean move(Shark[][] map) {
         if (this.j < map[0].length - 1) {
             this.j++;
             return true;
@@ -88,14 +56,13 @@ class King {
         return false;
     }
 
-    public void fishing() {
+    public void fishing(Shark[][] map) {
         for (int i = 1; i < map.length; i++) {
-            if (map[i][this.j].isEmpty()) continue;
+            if (map[i][this.j] == null) continue;
             
-            Shark shark = map[i][this.j].get(0);
+            Shark shark = map[i][this.j];
             score += shark.size;
-            map[i][this.j].clear();
-            sharks.remove(shark);
+            map[i][this.j] = null;
             return;
         }
     }
@@ -105,24 +72,17 @@ class Shark {
     static final int UP = 1, DOWN = 2, RIGHT = 3, LEFT = 4;
 
     int i, j, speed, dir, size;
-    boolean moved;
 
-    List<Shark>[][] map;
-
-    int r, c;
-
-    public Shark(int i, int j, int speed, int dir, int size, List<Shark>[][] map) {
+    public Shark(int i, int j, int speed, int dir, int size) {
         this.i = i;
         this.j = j;
         this.speed = speed;
         this.dir = dir;
         this.size = size;
-        this.map = map;
-        this.r = map.length - 1;
-        this.c = map[0].length - 1;
     }
 
-    public void move() {
+    public void move(Shark[][] map) {
+        int r = map.length-1, c = map[0].length-1;
         int ni = this.i, nj = this.j;
 
         if (dir == UP || dir == DOWN) {
@@ -147,8 +107,10 @@ class Shark {
             }
         }
 
-        map[this.i][this.j].remove(this);
-        map[ni][nj].add(this);
+        if (map[ni][nj] != null && map[ni][nj].size > this.size)
+            return;
+
+        map[ni][nj] = this;
         this.i = ni;
         this.j = nj;
     }
