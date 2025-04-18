b5ab6bcb9bc939a70df5a9e2f46b51f054ecb7a7
Ahn Joon Sung <tjdtna01@naver.com>
2025-04-18 18:00:37 +0900

    [Gold II] Title: 플로이드 2, Time: 1112 ms, Memory: 237976 KB -BaekjoonHub

--- 백준/Gold/11780. 플로이드 2/플로이드 2.java
+++ 백준/Gold/11780. 플로이드 2/플로이드 2.java
@@ -10,17 +10,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        readInput(sc);
+        floydWarshall();
+        printDistanceMatrix();
+        printAllPaths();
+    }
+
+    static void readInput(Scanner sc) {
         n = sc.nextInt();
         m = sc.nextInt();
-        
-        dist = new int[n+1][n+1];
-        next = new int[n+1][n+1];
 
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= n; j++) {
-                dist[i][j] = INF;
-                next[i][j] = -1;
-            }
+        dist = new int[n + 1][n + 1];
+        next = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            Arrays.fill(dist[i], INF);
+            Arrays.fill(next[i], -1);
             dist[i][i] = 0;
         }
 
@@ -34,16 +39,36 @@ public class Main {
                 next[from][to] = to;
             }
         }
+    }
 
-        floydWarshall();
+    static void floydWarshall() {
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                if (dist[i][k] == INF) continue;
+                for (int j = 1; j <= n; j++) {
+                    if (dist[k][j] == INF) continue;
+                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                        next[i][j] = next[i][k];
+                    }
+                }
+            }
+        }
+    }
 
+    static void printDistanceMatrix() {
         StringBuilder sb = new StringBuilder();
-        for (int from = 1; from <= n; from++) {
-            for (int to = 1; to <= n; to++) {
-                sb.append((dist[from][to] == INF ? 0 : dist[from][to]) + " ");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                sb.append(dist[i][j] == INF ? 0 : dist[i][j]).append(" ");
             }
             sb.append("\n");
         }
+        System.out.print(sb);
+    }
+
+    static void printAllPaths() {
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
@@ -53,7 +78,7 @@ public class Main {
                 } else {
                     sb.append(path.size()).append(" ");
                     for (int city : path) {
-                        sb.append((city)).append(" ");
+                        sb.append(city).append(" ");
                     }
                     sb.append("\n");
                 }
@@ -63,26 +88,8 @@ public class Main {
         System.out.print(sb);
     }
 
-    static void floydWarshall() {
-        for (int k = 1; k <= n; k++) {
-            for (int from = 1; from <= n; from++) {
-                if (dist[from][k] == INF) continue;
-
-                for (int to = 1; to <= n; to++) {
-                    if (dist[k][to] == INF) continue;
-
-                    if (dist[from][to] > dist[from][k] + dist[k][to]) {
-                        dist[from][to] = dist[from][k] + dist[k][to];
-                        next[from][to] = next[from][k];
-                    }
-                }
-            }
-        }
-    }
-
     static List<Integer> getPath(int from, int to) {
-        if (next[from][to] == -1)
-            return Collections.emptyList();
+        if (next[from][to] == -1) return Collections.emptyList();
 
         List<Integer> path = new ArrayList<>();
         while (from != to) {
@@ -90,7 +97,6 @@ public class Main {
             from = next[from][to];
         }
         path.add(to);
-
         return path;
     }
 }
